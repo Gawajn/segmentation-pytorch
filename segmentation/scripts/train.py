534969e19f823bbbdbd484c34f0dbd9715510d34
Alexander Hartelt <hartelt.alexander@gmail.com>
2023-01-11 15:57:01 +0100

    Updated train script

--- segmentation/scripts/train.py
+++ segmentation/scripts/train.py
@@ -12,7 +12,7 @@ from segmentation.callback import ModelWriterCallback, EarlyStoppingCallback
 from segmentation.losses import Losses
 from segmentation.metrics import Metrics, MetricReduction
 from segmentation.model_builder import ModelBuilderMeta, ModelBuilderLoad
-from segmentation.network import NetworkTrainer
+from segmentation.network import NetworkTrainer, Network
 from segmentation.preprocessing.workflow import PreprocessingTransforms, GrayToRGBTransform, ColorMapTransform, \
     NetworkEncoderTransform
 from segmentation.settings import Architecture, NetworkTrainSettings, Preprocessingfunction, ColorMap, ClassSpec
@@ -101,7 +101,7 @@ def parse_arguments():
                         default=ProcessingSettings.input_padding_value)
     parser.add_argument('--optimizer', default=NetworkTrainSettings.optimizer.value, type=str,
                         choices=[x.value for x in list(Optimizers)])
-    #metric settings
+    # metric settings
     parser.add_argument('--metrics', default=NetworkTrainSettings.default_metric(), type=str,
                         choices=[x.value for x in list(Metrics)], nargs='+')
     parser.add_argument('--metrics_watcher_index', default=0, type=int,
@@ -114,7 +114,8 @@ def parse_arguments():
 
     parser.add_argument('--loss', default=NetworkTrainSettings.loss.value, type=str,
                         choices=[x.value for x in list(Losses)])
-    parser.add_argument('--early_stopping', type=int, default=0,help="Number of epochs after which training is stopped model didn't improve")
+    parser.add_argument('--early_stopping', type=int, default=0,
+                        help="Number of epochs after which training is stopped model didn't improve")
     # Predefined
     parser.add_argument('--predefined_architecture',
                         default=get_default(PredefinedNetworkSettings, "architecture"),
@@ -185,10 +186,137 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def get_predefinedNetworkSettings(args, color_map: ColorMap):
+    return PredefinedNetworkSettings(architecture=Architecture(args.predefined_architecture),
+                                     encoder=args.predefined_encoder,
+                                     classes=len(color_map),
+                                     encoder_depth=args.predefined_encoder_depth,
+                                     decoder_channel=args.predefined_decoder_channel)
+
+
+def get_custom_model_settings(args, color_map: ColorMap) -> CustomModelSettings:
+    return CustomModelSettings(
+        encoder_filter=args.custom_model_encoder_filter,
+        decoder_filter=args.custom_model_decoder_filter,
+        attention_encoder_filter=args.custom_model_encoder_attention_filter,
+        attention=args.custom_model_use_attention,
+        classes=len(color_map),
+        attention_depth=args.custom_model_attention_depth,
+        encoder_depth=args.custom_model_encoder_depth,
+        attention_encoder_depth=args.custom_model_attention_encoder_depth,
+        stride=args.custom_model_stride,
+        padding=args.custom_model_padding,
+        kernel_size=args.custom_model_kernel_size,
+        weight_sharing=False if args.custom_model_no_weight_sharing else True,
+        scaled_image_input=args.custom_model_scaled_image_input,
+    )
+
+
+def build_model_from_args(args, color_map: ColorMap) -> Tuple[Network, ModelConfiguration]:
+    def remove_nones(x):
+        return [y for y in x if y is not None]
+
+    def default_transform():
+        result = albumentations.Compose([
+            albu.HorizontalFlip(),
+            albu.RandomGamma(),
+            albu.RandomBrightnessContrast(),
+            albu.OneOf([
+                albu.ToGray(),
+                albu.CLAHE()]),
+            albu.RandomScale(),
+
+        ])
+        return result
+
+    input_transforms = albumentations.Compose(remove_nones([
+        GrayToRGBTransform() if True else None,
+        ColorMapTransform(color_map=color_map.to_albumentation_color_map())
+
+    ]))
+    aug_transforms = default_transform()
+    tta_transforms = None
+    post_transforms = albumentations.Compose(remove_nones([
+        NetworkEncoderTransform(
+            args.predefined_encoder if not args.custom_model else Preprocessingfunction.name),
+        ToTensorV2()
+    ]))
+    transforms = PreprocessingTransforms(
+        input_transform=input_transforms,
+        aug_transform=aug_transforms,
+        # tta_transforms=tta_transforms,
+        post_transforms=post_transforms,
+    )
+
+    config = ModelConfiguration(use_custom_model=args.custom_model,
+                                network_settings=get_predefinedNetworkSettings(
+                                    args, color_map=color_map) if not args.custom_model else None,
+                                custom_model_settings=get_custom_model_settings(
+                                    args, color_map=color_map) if args.custom_model else None,
+                                preprocessing_settings=ProcessingSettings(transforms=transforms.to_dict(),
+                                                                          input_padding_value=args.padding_value,
+                                                                          rgb=True,
+                                                                          scale_max_area=args.scale_area,
+                                                                          preprocessing=Preprocessingfunction(
+                                                                              args.predefined_encoder if not args.custom_model else Preprocessingfunction.name)),
+                                color_map=color_map)
+
+    network = ModelBuilderMeta(config, args.device).get_model()
+
+    return network, config
+
+
+def build_model_from_loaded(args) -> Tuple[Network, ModelConfiguration]:
+    base_model_file = ModelBuilderLoad.from_disk(model_weights=args.load, device=args.device)
+    base_model = base_model_file.get_model()
+    base_config = base_model.get_model_configuration()
+    return base_model, base_config
+
+
+def train_arg(train, test, args, network: Network, config: ModelConfiguration, mask_settings: MaskSetting,
+              color_map: ColorMap, model_prefix="") -> Tuple[ModelWriterCallback, PreprocessingTransforms]:
+    transforms = PreprocessingTransforms.from_dict(network.proc_settings.transforms)
+
+    if args.mode == "xml_region" or args.mode == "xml_baseline":
+        dt = XMLDataset(train, transforms=transforms.get_train_transforms(),
+                        mask_generator=MaskGenerator(settings=mask_settings))
+        d_test = XMLDataset(test, transforms=transforms.get_test_transforms(),
+                            mask_generator=MaskGenerator(settings=mask_settings))
+
+    else:
+        dt = MaskDataset(train, transforms=transforms.get_train_transforms(), scale_area=args.scale_area)
+        d_test = MaskDataset(test, transforms=transforms.get_test_transforms(), scale_area=args.scale_area)
+
+    train_loader = DataLoader(dataset=dt, batch_size=1)
+    val_loader = DataLoader(dataset=d_test, batch_size=1)
+    mw = ModelWriterCallback(network, config, save_path=Path(args.output_path), prefix=model_prefix,
+                             metric_watcher_index=args.metrics_watcher_index)
+
+    callbacks = [mw]
+    if args.early_stopping > 0:
+        me = EarlyStoppingCallback(patience=args.early_stopping, metric_watcher_index=args.metrics_watcher_index)
+        callbacks.append(me)
+    trainer = NetworkTrainer(network, NetworkTrainSettings(classes=len(color_map),
+                                                           optimizer=Optimizers(args.optimizer),
+                                                           learningrate_seghead=args.learning_rate,
+                                                           batch_accumulation=args.batch_accumulation,
+                                                           processes=args.processes,
+                                                           metrics=[Metrics(x) for x in args.metrics],
+                                                           metric_reduction=MetricReduction(args.metrics_reduction),
+                                                           watcher_metric_index=args.metrics_watcher_index,
+                                                           class_weights=args.metrics_weights,
+                                                           loss=Losses(args.loss),
+                                                           ), args.device,
+                             callbacks=callbacks, debug_color_map=config.color_map)
+
+    trainer.train_epochs(train_loader=train_loader, val_loader=val_loader, n_epoch=args.n_epoch, lr_schedule=None)
+    return mw, transforms
+
+
 def main():
     args = parse_arguments()
-    train = dirs_to_pandaframe(args.train_input, args.train_mask)[:100]
-    test = dirs_to_pandaframe(args.test_input, args.test_mask)[:5]
+    train = dirs_to_pandaframe(args.train_input, args.train_mask)
+    test = dirs_to_pandaframe(args.test_input, args.test_mask)
     device = args.device
 
     if len(test) == 0:
@@ -203,118 +331,16 @@ def main():
                               ClassSpec(label=2, name="BaselineBorder", color=[0, 255, 0])])
     else:
         raise NotImplementedError()
-
-    from segmentation.dataset import default_transform
     mask_settings = MaskSetting(MASK_TYPE=MaskType.BASE_LINE if args.mode == "xml_baseline" else MaskType.ALLTYPES,
                                 PCGTS_VERSION=PCGTSVersion.PCGTS2013, LINEWIDTH=5,
                                 BASELINELENGTH=10)
 
-    def get_predefinedNetworkSettings(args):
-        return PredefinedNetworkSettings(architecture=Architecture(args.predefined_architecture),
-                                         encoder=args.predefined_encoder,
-                                         classes=len(color_map),
-                                         encoder_depth=args.predefined_encoder_depth,
-                                         decoder_channel=args.predefined_decoder_channel)
+    if args.finetune and args.load:
+        network, config = build_model_from_loaded(args)
 
-    def get_custom_model_settings(args) -> CustomModelSettings:
-        return CustomModelSettings(
-            encoder_filter=args.custom_model_encoder_filter,
-            decoder_filter=args.custom_model_decoder_filter,
-            attention_encoder_filter=args.custom_model_encoder_attention_filter,
-            attention=args.custom_model_use_attention,
-            classes=len(color_map),
-            attention_depth=args.custom_model_attention_depth,
-            encoder_depth=args.custom_model_encoder_depth,
-            attention_encoder_depth=args.custom_model_attention_encoder_depth,
-            stride=args.custom_model_stride,
-            padding=args.custom_model_padding,
-            kernel_size=args.custom_model_kernel_size,
-            weight_sharing=False if args.custom_model_no_weight_sharing else True,
-            scaled_image_input=args.custom_model_scaled_image_input,
-        )
-
-    def train_arg(train, test, args, model_prefix="") -> Tuple[ModelWriterCallback, PreprocessingTransforms]:
-        def remove_nones(x):
-            return [y for y in x if y is not None]
-
-        def default_transform():
-            result = albumentations.Compose([
-                albu.HorizontalFlip(),
-                albu.RandomGamma(),
-                albu.RandomBrightnessContrast(),
-                albu.OneOf([
-                    albu.ToGray(),
-                    albu.CLAHE()]),
-                albu.RandomScale(),
-
-            ])
-            return result
-        input_transforms = albumentations.Compose(remove_nones([
-            GrayToRGBTransform() if True else None,
-            ColorMapTransform(color_map=color_map.to_albumentation_color_map())
-
-        ]))
-        aug_transforms = default_transform()
-        tta_transforms = None
-        post_transforms = albumentations.Compose(remove_nones([
-            NetworkEncoderTransform(args.predefined_encoder if not args.custom_model else Preprocessingfunction.name),
-            ToTensorV2()
-        ]))
-        transforms = PreprocessingTransforms(
-            input_transform=input_transforms,
-            aug_transform=aug_transforms,
-            #tta_transforms=tta_transforms,
-            post_transforms=post_transforms,
-        )
-
-        if args.mode == "xml_region" or args.mode == "xml_baseline":
-            dt = XMLDataset(train, transforms=transforms,
-                            mask_generator=MaskGenerator(settings=mask_settings))
-            d_test = XMLDataset(test, transforms=transforms,
-                                mask_generator=MaskGenerator(settings=mask_settings))
-
-        else:
-            dt = MaskDataset(train, transforms=transforms, scale_area=args.scale_area)
-            d_test = MaskDataset(test, transforms=transforms, scale_area=args.scale_area)
-
-        train_loader = DataLoader(dataset=dt, batch_size=1)
-        val_loader = DataLoader(dataset=d_test, batch_size=1)
-
-        config = ModelConfiguration(use_custom_model=args.custom_model,
-                                    network_settings=get_predefinedNetworkSettings(
-                                        args) if not args.custom_model else None,
-                                    custom_model_settings=get_custom_model_settings(
-                                        args) if args.custom_model else None,
-                                    preprocessing_settings=ProcessingSettings(transforms=transforms.to_dict(),
-                                                                              input_padding_value=args.padding_value,
-                                                                              rgb=True,
-                                                                              scale_max_area=args.scale_area,
-                                                                              preprocessing=Preprocessingfunction(
-                                                                                  args.predefined_encoder if not args.custom_model else Preprocessingfunction.name)),
-                                    color_map=color_map)
-
-        network = ModelBuilderMeta(config, args.device).get_model()
-        mw = ModelWriterCallback(network, config, save_path=Path(args.output_path), prefix=model_prefix, metric_watcher_index=args.metrics_watcher_index)
-
-        callbacks = [mw]
-        if args.early_stopping > 0:
-            me = EarlyStoppingCallback(patience=args.early_stopping, metric_watcher_index=args.metrics_watcher_index)
-            callbacks.append(me)
-        trainer = NetworkTrainer(network, NetworkTrainSettings(classes=len(color_map),
-                                                               optimizer=Optimizers(args.optimizer),
-                                                               learningrate_seghead=args.learning_rate,
-                                                               batch_accumulation=args.batch_accumulation,
-                                                               processes=args.processes,
-                                                               metrics=[Metrics(x) for x in args.metrics],
-                                                               metric_reduction=MetricReduction(args.metrics_reduction),
-                                                               watcher_metric_index=args.metrics_watcher_index,
-                                                               class_weights=args.metrics_weights,
-                                                               loss=Losses(args.loss),
-                                                               ), args.device,
-                                 callbacks=callbacks, debug_color_map=config.color_map)
-
-        trainer.train_epochs(train_loader=train_loader, val_loader=val_loader, n_epoch=args.n_epoch, lr_schedule=None)
-        return mw, transforms
+    else:
+        network, config = build_model_from_args(args, color_map=color_map)
+        from segmentation.dataset import default_transform
 
     model_writers = []
     test_transforms = []
@@ -323,12 +349,14 @@ def main():
         for ind, x in enumerate(kf.split(train, None)):
             train_f = train.iloc[x[0]].reset_index(drop=True)
             test_f = train.iloc[x[1]].reset_index(drop=True)
-            mw, tt = train_arg(train_f, test_f, args=args, model_prefix=f"fold{ind}")
+            mw, tt = train_arg(train_f, test_f, args=args, model_prefix=f"fold{ind}", config=config, network=network,
+                               mask_settings=mask_settings, color_map=color_map)
             model_writers.append(mw)
             test_transforms.append(tt)
 
     else:
-        mw, tt = train_arg(train=train, test=test, args=args)
+        mw, tt = train_arg(train=train, test=test, args=args, network=network, config=config,
+                           mask_settings=mask_settings, color_map=color_map)
         model_writers.append(mw)
         test_transforms.append(tt)
 
@@ -342,13 +370,14 @@ def main():
         elif args.eval_images:
             total_accuracy = 0
             total_loss = 0
-            for ind, (mw,tt) in enumerate(model_writers):
+            for ind, (mw, tt) in enumerate(model_writers):
                 mw: ModelWriterCallback = mw
                 tt: PreprocessingTransforms = tt
                 ml = ModelBuilderLoad.from_disk(mw.get_best_model_path(), device=device).get_model()
                 eval_df = dirs_to_pandaframe(args.eval_input, args.eval_mask)
                 if args.mode == "xml_region" or args.mode == "xml_baseline":
-                    d_eval = XMLDataset(eval_df, transforms=tt.get_test_transforms(), scale_area=args.scale_area,  mask_generator=MaskGenerator(settings=mask_settings))
+                    d_eval = XMLDataset(eval_df, transforms=tt.get_test_transforms(), scale_area=args.scale_area,
+                                        mask_generator=MaskGenerator(settings=mask_settings))
 
                 else:
                     d_eval = MaskDataset(eval_df, transforms=tt.get_test_transforms(),
@@ -356,8 +385,13 @@ def main():
                 eval_loader = DataLoader(dataset=d_eval, batch_size=1)
 
                 from segmentation.network import test as test_network
-                accuracy, loss = test_network(ml.model, device, eval_loader, Losses(args.loss).get_loss()() if Losses(args.loss) == Losses.cross_entropy_loss else Losses(args.loss).get_loss()(mode="multiclass"), classes=len(color_map),
-                                              metrics=[Metrics(x) for x in args.metrics], metric_reduction=MetricReduction(args.metrics_reduction),  metric_watcher_index=args.metrics_watcher_index, class_weights=args.metrics_weights,
+                accuracy, loss = test_network(ml.model, device, eval_loader, Losses(args.loss).get_loss()() if Losses(
+                    args.loss) == Losses.cross_entropy_loss else Losses(args.loss).get_loss()(mode="multiclass"),
+                                              classes=len(color_map),
+                                              metrics=[Metrics(x) for x in args.metrics],
+                                              metric_reduction=MetricReduction(args.metrics_reduction),
+                                              metric_watcher_index=args.metrics_watcher_index,
+                                              class_weights=args.metrics_weights,
                                               padding_value=args.padding_value)
                 total_accuracy += accuracy.stats[args.metrics_watcher_index].value()
                 total_loss += loss
